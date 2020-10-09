847b0b13118da91c4f5d9ba7d7964049d42638f7
Zayac11 <podolskiy.2001@mail.ru>
2020-10-09 23:39:18 +0300

    added car chosing page

--- screens/Brand.js
+++ screens/Brand.js
@@ -1,24 +1,109 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import carLogo from '../assets/car.png'
 
 const BrandScreen = ({navigation}) => (
-    <View style={styles.container}>
-        <Text>Brand</Text>
-        <Button 
-        title = "Go to body"
-        onPress = {
-            () => navigation.navigate('body')
-        }
-        />
-    </View>
+        
+    <View style={styles.backContainer}>
+        <View style={styles.contentContainer}>
+            <View style={styles.carContainer}>
+                <Image 
+                    style={styles.tinyLogo}
+                    source={carLogo}
+                />
+                <Text style={styles.titleText}>BWM 5</Text>
+                <Text style={styles.carText}>Description</Text>
+            </View>
+            <View style={styles.creditContainer}>
+                <TouchableOpacity style={styles.buyButton}
+                    onPress = {
+                        () => navigation.navigate('model')
+                    }
+                >
+                    <Text style={styles.buyText}>Посмотреть предложения</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.creditButton}
+                    onPress = {
+                        () => navigation.navigate('price')
+                    }
+                >
+                    <Text style={styles.creditText}>Это не тот автомобиль</Text>
+                </TouchableOpacity>
+            </View>
+        </View>               
+    </View>    
 )
 
 const styles = StyleSheet.create({
-    container: {
+    contentContainer: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        marginTop: 30,
+        width: '100%',
+        height: '85%'
+    },
+    backContainer: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: 'center'
-    }
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        paddingHorizontal: 15,
+        width: '100%'
+    },
+    container: {
+        flex: 5,
+        backgroundColor: '#fff',
+        borderRadius: 20
+    },  
+    bottomContainer: {
+        flex: 1
+    },
+    titleText: {
+        paddingTop: 15,
+        fontWeight: 'bold',
+        fontSize: 24
+    },
+    carText: {
+        fontSize: 18,
+        color: '#999ea9'
+    },
+    carContainer: {
+        flex: 2,
+        justifyContent: 'center',
+		alignItems: 'center'
+    },
+    creditContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 15
+    },
+    buyButton: {
+        width: '100%',
+        borderRadius: 10,
+        backgroundColor: '#3a83f1',
+        alignItems: 'center',
+        paddingVertical: 15,
+        marginBottom: 25
+    },
+    buyText: {
+        textTransform: "uppercase",
+        fontSize: 15,
+        letterSpacing: 1.5,
+        color: '#fff'
+    },
+    creditButton: {
+        width: '100%',
+        alignItems: 'center',
+        // marginBottom: 15
+    },
+    creditText: {
+        textTransform: "uppercase",
+        fontSize: 15,
+        letterSpacing: 1.5,
+        color: '#3a83f1'
+    },
+    tinyLogo: {
+        width: 250,
+        height: 210,
+      },
 })
 
 export default BrandScreen;
--- screens/Price.js
+++ screens/Price.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
 
 const PriceScreen = ({navigation}) => (
-    <View>
+    <View style={styles.container}>
         <Text>Price</Text>
     </View>
 )
